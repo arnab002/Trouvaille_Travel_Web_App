9cde92e23c25fd94b5bdbe42e419cfb6e139f53e
ARNAB MODAK <142472616+arnab002@users.noreply.github.com>
2024-06-29 11:29:41 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -25,10 +25,7 @@ mongoose.set("strictQuery", false);
 
 async function connect() {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB Database Connected");
   } catch (err) {
